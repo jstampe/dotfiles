0e7a655d2933cd1a52869230e3bf78bbab161392
jstampe <jstampe@gmail.com>
2019-09-09 11:22:11 +0200

    Added gitconfig and agignore

--- copy-files.sh
+++ copy-files.sh
@@ -5,6 +5,10 @@ cp ~/.bashrc ./dot-bashrc
 cp ~/.screenrc ./dot-screenrc
 cp ~/.inputrc ./dot-inputrc
 cp ~/.vimrc ./dot-vimrc
+cp ~/.agignore ./dot-agignore
+
+# Git
+cat ~/.gitconfig | grep -v user | grep -v jstampe > ./dot-gitconfig
 
 # VSCode
 cp -r ~/.config/Code/User/snippets/ ./VSCode/
